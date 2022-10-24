d481c71c797d9d36e6b9f4b222ff49f6373e05bd
Tuan Anh Le <20021286@vnu.edu.vn>
2022-10-24 14:26:00 +0700

    add booking card

---
+++ traveloka/lib/entity/booking.dart
@@ -0,0 +1,8 @@
+class BookingHotel {
+  late DateTime bookingFromDate;
+  late DateTime bookingToDate;
+  late DateTime bookingTimestamp;
+  late String userId;
+  late String hotelId;
+  late int price;
+}
